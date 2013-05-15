ce7c5e19364b569f6c2486f8515fbb3eb729a00d
s3rv4i <s3rv4i@freesyria.ch>
2013-05-16 01:25:02 +0200

    Favicon fix - finally?

--- app.js
+++ app.js
@@ -28,8 +28,6 @@ app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.favicon(__dirname + '/public/favicon.ico')); 
-
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
